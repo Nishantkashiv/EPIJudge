8511215e182db958fe586c9b64334bd281d7c446
Nishant Kashiv <nishantkashiv@live.com>
2023-12-16 17:43:41 -0700

    sorted_lists_merge.py

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,7 +6,35 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
+    if not L1:
+        return L2
+    elif not L2:
+        return L1
+    result = None
+    if L1.data <= L2.data:
+        result = L1
+        L1 = L1.next
+    else:
+        result = L2
+        L2 = L2.next
+    result_current = result
+    while L1 or L2:
+        if L1 and L2:
+            if L1.data <= L2.data:
+                result_current.next = L1
+                L1 = L1.next
+            else:
+                result_current.next = L2
+                L2 = L2.next
+            result_current = result_current.next
+        elif L1:
+            result_current.next = L1
+            return result
+        elif L2:
+            result_current.next = L2
+            return result
+    return result
+
     return None
 
 
