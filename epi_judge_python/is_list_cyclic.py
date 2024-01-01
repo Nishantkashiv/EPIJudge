267c9b775e55b56d006c8d88e3208ee33d0c5df7
Nishant Kashiv <nishantkashiv@live.com>
2024-01-01 00:56:38 -0700

    Complete detection of cycle in linked list using the hare tortoise method

--- epi_judge_python/is_list_cyclic.py
+++ epi_judge_python/is_list_cyclic.py
@@ -8,9 +8,54 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def has_cycle(head: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
+    """
+    Two iterators slow and fast.
+    Slow will move one step at a time. 
+    Fast will move 2 steps at a time.
+    If Fast reaches null return false
+    If slow and fast are at the same node return true
+    """
+    fast = slow = head
+    cycle_length = 0
+    while fast and fast.next is not None and fast.next.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow is fast:
+            """
+            This means that the list contains a cycle
+            Now calculate the lenght of the cycle
+            """
+            cycle_length = calculate_cycle_length(slow)
+            return calculate_start_node(head, cycle_length)
     return None
 
+
+def calculate_start_node(head: ListNode, cycle_length: int) -> ListNode: 
+    it = head
+    while True:
+        it_plus_cycle = it
+        for _ in range(cycle_length):
+            it_plus_cycle = it_plus_cycle.next
+        if it_plus_cycle is it:
+            return it
+        it = it.next
+    return
+
+
+def calculate_cycle_length(head: ListNode) -> int:
+    """
+    Keep a while loop with increasing cycle length
+    when node + c hops == current node that is the cycle length
+    """
+    c = 2
+    while True:
+        current = head
+        for _ in range(c):
+            current = current.next
+        if current is head:
+            return c
+        c += 1
+    return
 
 @enable_executor_hook
 def has_cycle_wrapper(executor, head, cycle_idx):
